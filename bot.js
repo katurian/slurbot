783722dc62f0595f471209a611f97bd437a85ab8
Kate Kulinski <35544223+katurian@users.noreply.github.com>
2019-03-31 01:13:42 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,35 +7,95 @@ client.on('message', (receivedMessage) => {
 }
 
 // Check if the bot's user was tagged in the message
-if (receivedMessage.content.toLowerCase().includes("whore")) {
+if (receivedMessage.content.toLowerCase().replace(/\s/g, '').includes("whore")) {
     receivedMessage.delete(1000);
 }
 
-if (receivedMessage.content.toLowerCase().includes("nigger")) {
+if (receivedMessage.content.toLowerCase().replace(/\./g, '').includes("whore")) {
     receivedMessage.delete(1000);
 }
 
-if (receivedMessage.content.toLowerCase().includes("kike")) {
+if (receivedMessage.content.toLowerCase().replace(/\./g, '').includes("nigger")) {
     receivedMessage.delete(1000);
 }
 
-if (receivedMessage.content.toLowerCase().includes("slut")) {
+if (receivedMessage.content.toLowerCase().replace(/\s/g, '').includes("nigger")) {
     receivedMessage.delete(1000);
 }
 
-if (receivedMessage.content.toLowerCase().includes("manlet")) {
+if (receivedMessage.content.toLowerCase().replace(/\./g, '').includes("kike")) {
     receivedMessage.delete(1000);
 }
 
-if (receivedMessage.content.toLowerCase().includes("brainlet")) {
+if (receivedMessage.content.toLowerCase().replace(/\s/g, '').includes("kike")) {
     receivedMessage.delete(1000);
 }
 
-if (receivedMessage.content.toLowerCase().includes("cunt")) {
+if (receivedMessage.content.toLowerCase().replace(/\./g, '').includes("slut")) {
     receivedMessage.delete(1000);
 }
 
-if (receivedMessage.content.toLowerCase().includes("manchild")) {
+if (receivedMessage.content.toLowerCase().replace(/\s/g, '').includes("slut")) {
+    receivedMessage.delete(1000);
+}
+
+if (receivedMessage.content.toLowerCase().replace(/\./g, '').includes("manlet")) {
+    receivedMessage.delete(1000);
+}
+
+if (receivedMessage.content.toLowerCase().replace(/\s/g, '').includes("manlet")) {
+    receivedMessage.delete(1000);
+}
+
+if (receivedMessage.content.toLowerCase().replace(/\./g, '').includes("brainlet")) {
+    receivedMessage.delete(1000);
+}
+
+if (receivedMessage.content.toLowerCase().replace(/\s/g, '').includes("brainlet")) {
+    receivedMessage.delete(1000);
+}
+
+if (receivedMessage.content.toLowerCase().replace(/\./g, '').includes("brainlet")) {
+    receivedMessage.delete(1000);
+}
+
+if (receivedMessage.content.toLowerCase().replace(/\s/g, '').includes("brainlet")) {
+    receivedMessage.delete(1000);
+}
+
+if (receivedMessage.content.toLowerCase().replace(/\./g, '').includes("manchild")) {
+    receivedMessage.delete(1000);
+}
+
+if (receivedMessage.content.toLowerCase().replace(/\./g, '').includes("manchild")) {
+    receivedMessage.delete(1000);
+}
+
+if (receivedMessage.content.toLowerCase().replace(/\s/g, '').includes("kïke")) {
+    receivedMessage.delete(1000);
+}
+
+if (receivedMessage.content.toLowerCase().replace(/\./g, '').includes("kïke")) {
+    receivedMessage.delete(1000);
+}
+
+if (receivedMessage.content.toLowerCase().replace(/\s/g, '').includes("faggot")) {
+    receivedMessage.delete(1000);
+}
+
+if (receivedMessage.content.toLowerCase().replace(/\./g, '').includes("faggot")) {
+    receivedMessage.delete(1000);
+}
+
+if (receivedMessage.content.toLowerCase().replace(/\s/g, '').includes("fag")) {
+    receivedMessage.delete(1000);
+}
+
+if (receivedMessage.content.toLowerCase().replace(/\./g, '').includes("fag")) {
+    receivedMessage.delete(1000);
+}
+
+if (receivedMessage.content.toLowerCase().includes("n1gger")) {
     receivedMessage.delete(1000);
 }
 
